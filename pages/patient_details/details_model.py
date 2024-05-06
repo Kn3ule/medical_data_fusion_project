7d9e9e2d202e8fae38f523f6e13afde99901320c
Tim Konle <tim.konle@web.de>
2024-05-06 20:07:06 +0200

    added function to load patient data with id

--- pages/patient_details/details_model.py
+++ pages/patient_details/details_model.py
@@ -5,3 +5,8 @@ import pandas as pd
 import wfdb
 
 from models import engine
+
+# Load locations from database
+def load_patient_data(id):
+    return pd.read_sql(f"""SELECT * FROM metadata where patient_id = {id}
+;""", engine)
